c503dff716e0ada792ea35f72f42ef6ed5099a4d
Davee Sok <daveesok@gmail.com>
2021-07-31 21:16:56 -0500

    about to check into new branch

--- web_scraper/web_scraper.py
+++ web_scraper/web_scraper.py
@@ -5,22 +5,26 @@ import time
 sub_string = "citation needed"
 url = "https://en.wikipedia.org/wiki/History_of_Mexico"
 res = requests.get(url)
+
+print(res)
 list_with_citation = []
-soup = BeautifulSoup(res.content, "html.parser")
 
-pararaph_list = soup.find_all("p")
 
-for item in pararaph_list:
-    for string in item.stripped_strings:
-        if sub_string in repr(string):
-            list_with_citation.append(item.text)
+# soup = BeautifulSoup(res.content, "html.parser")
 
-print("Number of citations needed: ", len(list_with_citation))
-print()
+# pararaph_list = soup.find_all("p")
 
-for paragraph in list_with_citation:
-    # print('Original Paragraph \n',paragraph)
-    head, sep, tail = paragraph.partition("[")
-    print("Stripped Paragraph \n", head)
-    print()
-    time.sleep(4)
+# for item in pararaph_list:
+#     for string in item.stripped_strings:
+#         if sub_string in repr(string):
+#             list_with_citation.append(item.text)
+
+# print("Number of citations needed: ", len(list_with_citation))
+# print()
+
+# for paragraph in list_with_citation:
+#     # print('Original Paragraph \n',paragraph)
+#     head, sep, tail = paragraph.partition("[")
+#     print("Stripped Paragraph \n", head)
+#     print()
+#     time.sleep(4)
